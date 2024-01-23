03fe158426f0640d69be98d201857e20b8261719
aka.rlova <ladyerial88@gmail.com>
2024-01-24 01:11:36 +0200

    Update CalcController methods

--- src/app/CalcController.java
+++ src/app/CalcController.java
@@ -5,12 +5,18 @@ public class CalcController {
     CalcView view = new CalcView();
 
     public void getRes() {
+        int option = view.getOpt();
         double value = view.getData();
-        view.getOutput(getOptRes(value));
+        view.getOutput(getOptRes(option,value));
 
     }
 
-    private String getOptRes(double value) {
-        return new MilesService().milesToKilometers(value);
-    }
+    private String getOptRes(int option, double value) {
+        String res = "";
+        switch (option) {
+            case 1 -> res = new MilesService().milesToKilometers(value);
+            case 2 -> res = new KilometersService().kilometersToMile(value);
+            case 0 -> res = Constants.APP_CLOSE_MSG;
+        }
+        return res;
 }
